9b4a4c173dd182e59f4b7653bbfd3737481ce3a7
Bastien Geldreich <bastien.geldreich@free.fr>
2018-12-12 09:31:40 +0100

    ctoi le noob

---
+++ src/service/UserService.java
@@ -0,0 +1,25 @@
+package service;
+
+import java.util.List;
+
+import javax.ws.rs.FormParam;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+
+@Path("/user")
+public class UserService {
+
+		@POST
+		@Path("/add")
+		@Produces(MediaType.APPLICATION_JSON)
+		public Response addUser(@FormParam("id") String id, @FormParam("pwd") String pwd) {
+			System.out.println(id+" "+pwd);
+			return Response.ok().build();
+		}
+
+}
