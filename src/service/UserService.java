b15ebd50faab0bf866aa71d9f9a3d1765e9ef8a5
Linkdu40 <lucas.gauziede@hotmail.fr>
2018-12-12 11:19:47 +0100

    Patch bug

--- src/service/UserService.java
+++ src/service/UserService.java
@@ -1,35 +1,6 @@
-
 package service;
 
-import java.util.List;
-
 import javax.ws.rs.FormParam;
-import javax.ws.rs.GET;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
-
-
-@Path("/user")
-public class UserService {
-
-		@POST
-		@Path("/add")
-		@Produces(MediaType.APPLICATION_JSON)
-		public Response addUser(@FormParam("id") String id, @FormParam("pwd") String pwd) {
-			System.out.println(id+" "+pwd);
-			return Response.ok().build();
-		}
-
-}
-package service;
-
-import java.util.List;
-
-import javax.ws.rs.FormParam;
-import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -71,4 +42,5 @@ public class UserService {
 			System.out.println(id+" "+pwd);
 			return Response.ok().build();
 		}
+
 }
